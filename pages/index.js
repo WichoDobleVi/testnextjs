c9c8dc87f3dab1bc86a14d578d5ac9e1885997fb
WichoDobleVi <hh2vi@hotmail.com>
2021-10-08 16:42:13 -0500

    first

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+
+export default function Home(props) {
+
+    return (
+        <div>
+            <h2>Home</h2>
+            <style jsx>
+                { `
+                    h2 {
+                        color: red;
+                    }
+                 
+                 `
+                }
+            </style>
+        </div>
+    )
+}
+
+export async function getServerSideProps(context) {
+    console.log(context);
+    const {mac, apmac} = context.query
+    const apmac_ = apmac.replace(/:/g, '');
+    const res = await fetch(`https://sharedfi-prod.uc.r.appspot.com/aps?apMac=${apmac_}`);
+    const aps = await res.json();
+    return {
+        props: {
+            aps
+        }
+    }
+}
